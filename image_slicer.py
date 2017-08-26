6d67e0e2b295ea23335397adb235c4e176254ff2
Martin Clifford <mclifford82@gmail.com>
2017-08-26 14:23:05 -0700

    image_slicer.py

---
+++ image_slicer.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import os
+import math
+
+export_path = "C:\\py\\image_slicer\\output/"
+image = Image.open("C:/py/image_slicer/input.jpg")
+imgwidth, imgheight = image.size
+iterations = math.floor(imgwidth / imgheight) + 1
+
+print("Image height: %s " % imgheight)
+print("Image width: %s " % imgwidth)
+print("Iterations: %s " % iterations)
+print("")
+
+for i in range(0, iterations):
+    save_path = "C:\\py\\image_slicer\\img-%s.png" % i
+    box = (i*imgheight, 0, min((i+1)*imgheight, imgwidth), imgheight)
+    a = image.crop(box)
+    
+    # Debug Info
+    newwidth, newheight = a.size
+    print("Box {}: {}, width: {}, height: {}".format(i+1, box, newwidth, newheight))
+    print("Save path: {}".format(save_path))
+
+    try:
+        a.save(save_path)
+    except:
+        print("Could not save image slice.")
